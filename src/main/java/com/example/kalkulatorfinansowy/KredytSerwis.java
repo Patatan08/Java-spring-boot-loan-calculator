c511767de0fe2da241e558a0547e419d914b1116
Patryk Tomaszek <patryktomaszek6@gmail.com>
2026-01-20 09:40:11 +0100

    Service Logic

--- src/main/java/com/example/kalkulatorfinansowy/KredytSerwis.java
+++ src/main/java/com/example/kalkulatorfinansowy/KredytSerwis.java
@@ -1,4 +1,23 @@
 package com.example.kalkulatorfinansowy;
 
-public class KredytSerwis   {
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+
+@Service
+public class KredytSerwis {
+
+    public BigDecimal obliczRate(BigDecimal sum, BigDecimal LoanInterest, int months) {
+        BigDecimal years = new BigDecimal(months).divide(new BigDecimal(12), 10, BigDecimal.ROUND_HALF_UP);
+
+        BigDecimal interest = sum
+                .multiply(LoanInterest)
+                .divide(new BigDecimal(100), 10, BigDecimal.ROUND_HALF_UP)
+                .multiply(years);
+
+        BigDecimal total = total.add(interest);
+
+        BigDecimal installment = total.divide(new BigDecimal(months), 2, BigDecimal.ROUND_HALF_UP);
+
+        return installment;
+    }
 }
