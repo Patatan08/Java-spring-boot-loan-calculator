9c8e0ba369c3b8be768910d7429187e85d7a68bd
Patryk Tomaszek <patryktomaszek6@gmail.com>
2026-01-22 09:34:16 +0100

    Application error handling

--- src/main/java/com/example/kalkulatorfinansowy/KalkulatorFinansowyApplication.java
+++ src/main/java/com/example/kalkulatorfinansowy/KalkulatorFinansowyApplication.java
@@ -6,9 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KalkulatorFinansowyApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+    try {
         SpringApplication.run(KalkulatorFinansowyApplication.class, args);
+    System.out.println("Aplikacja uruchomiona");
     }
+    catch (Exception e){
+        System.err.println("Aplikacja nie zostałą poprawnie uruchomiona");
+        throw e;
 
+        }
+    }
 }
 
